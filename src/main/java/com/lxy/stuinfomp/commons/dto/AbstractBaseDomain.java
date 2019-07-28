2e8442b11dda0af353e802607fe23a37ceced534
robin_time <robin_time@163.com>
2019-07-28 17:44:17 +0800

    用户注册接口

--- src/main/java/com/lxy/stuinfomp/commons/dto/AbstractBaseDomain.java
+++ src/main/java/com/lxy/stuinfomp/commons/dto/AbstractBaseDomain.java
@@ -1,10 +1,33 @@
 package com.lxy.stuinfomp.commons.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 public abstract class AbstractBaseDomain implements Serializable {
+    /**
+     * 主键ID
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String creator;
+
+    @Column(name = "gmt_created")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date gmtCreated;
+
+    private String modifier;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @Column(name = "gmt_modified")
+    private Date gmtModified;
 }
