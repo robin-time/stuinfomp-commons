758001aefc01992b91ab6aca2d947aa3fb097ccd
robin_time <robin_time@163.com>
2019-07-28 15:55:51 +0800

    架构公共部分优化

--- src/main/java/com/lxy/stuinfomp/commons/dto/BaseResultFactory.java
+++ src/main/java/com/lxy/stuinfomp/commons/dto/BaseResultFactory.java
@@ -1,5 +1,6 @@
 package com.lxy.stuinfomp.commons.dto;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 public class BaseResultFactory<T extends AbstractBaseDomain> {
@@ -10,11 +11,13 @@ public class BaseResultFactory<T extends AbstractBaseDomain> {
 
     private static BaseResultFactory baseResultFactory;
 
+    private static HttpServletResponse response;
+
     /**
      * 单例设计模式
      * @return
      */
-    public static BaseResultFactory getInstance(){
+    public static BaseResultFactory getInstance(HttpServletResponse response){
         if (baseResultFactory == null){
             synchronized (BaseResultFactory.class){
                 if (baseResultFactory == null){
@@ -22,6 +25,8 @@ public class BaseResultFactory<T extends AbstractBaseDomain> {
                 }
             }
         }
+        BaseResultFactory.response = response;
+        baseResultFactory.initResponse();
         return baseResultFactory;
     }
 
@@ -55,11 +60,16 @@ public class BaseResultFactory<T extends AbstractBaseDomain> {
      * @param level 日志级别，如果是debug模式，就显示detail详情，否则是null
      * @return
      */
-    public static AbstractBaseResult build(int code,String title,String detail,String level){
+    public  AbstractBaseResult build(int code,String title,String detail,String level){
+        response.setStatus(code); //设置请求失败的响应码
        if(LOGGER_LEVEL_DEBUG.equals(level.toUpperCase())){
            return new ErrorResult(code,title,detail);
        }else {
            return new ErrorResult(code,title,null);
        }
+    }
+
+    private void initResponse(){
+        response.setHeader("Content-Type","application/vnd.api+json");
     }
 }
---
+++ src/main/java/com/lxy/stuinfomp/commons/web/AbstractBaseController.java
@@ -0,0 +1,46 @@
+package com.lxy.stuinfomp.commons.web;
+
+import com.lxy.stuinfomp.commons.dto.AbstractBaseDomain;
+import com.lxy.stuinfomp.commons.dto.AbstractBaseResult;
+import com.lxy.stuinfomp.commons.dto.BaseResultFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+public abstract class AbstractBaseController<T extends AbstractBaseDomain> {
+
+    private static final String LOGGING_LEVEL_MY_SHOP = "logging.level.com.lxy.stuinfomp";
+
+    protected HttpServletRequest request;
+    protected HttpServletResponse response;
+
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    @ModelAttribute
+    public void initReqAndRes (HttpServletRequest request,HttpServletResponse response){
+        this.request  = request;
+        this.response = response;
+    }
+
+    protected AbstractBaseResult success(String self,T attribute){
+        return BaseResultFactory.getInstance(response).build(self,attribute);
+    }
+
+    protected AbstractBaseResult success(String self, int next, int last, List<T> attributes){
+        return BaseResultFactory.getInstance(response).build(self,next,last,attributes);
+    }
+
+    protected AbstractBaseResult error(int code,String title,String detail){
+        return BaseResultFactory.getInstance(response).build(code,title,detail,applicationContext.getEnvironment().getProperty(LOGGING_LEVEL_MY_SHOP));
+    }
+
+    protected AbstractBaseResult error(String title,String detail){
+        return error(HttpStatus.UNAUTHORIZED.value(),title,detail);
+    }
+}
